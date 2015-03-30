32e00d4eaa77b44682f0fc3b02d340211ad836c9
David Tracy <davidptracy@gmail.com>
2015-03-29 22:36:45 -0400

    added rate counting

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ io.on('connection', function (socket){
 
 function emitMessage(object){
 	io.emit(object.name, object);
-	console.log("Sent "+object.name);
+	console.log("Sent " + object.name);
 }
 
 //========================================================
@@ -59,16 +59,28 @@ var client = new Twitter({
 	access_token_secret: '5QHtfe2T2N1hoEVJ4Cl4EsSR22OfCFVdxa8AxJ2OcpmHd'
 });
 
+var startTime = new Date().getTime();
+
+var timeBetweenTweets;
+var currentTime = 0;
+var previousTime = 0;
+var timeBuffer = [];
+var timeIncrement = 0;
+var rate;
+
+var BUFFER_LENGTH = 10;
+
 client.stream('statuses/filter', {track: 'ISIS'}, function(stream) {
+	
 	stream.on('data', function(tweet) {
+
 		// tweet is a JSON object
 		// console.log(tweet);
 		// console.log(tweet.user.screen_name);
-				
+						
 		var tweetObject = {
 			"name" : "tweet",
-			"text" : tweet.text,
-			"screenName" : tweet.user.screen_name
+			"rate" : calculateTweetRate()
 		} 
 
 		emitMessage(tweetObject);
@@ -79,3 +91,32 @@ client.stream('statuses/filter', {track: 'ISIS'}, function(stream) {
 		throw error;
 	});
 });
+
+function calculateTweetRate(){
+		rate = 0;
+		previousTime = currentTime;
+
+		currentTime = new Date().getTime();
+
+		if (timeBuffer.length == 0){
+			timeBetweenTweets = currentTime - startTime;
+		} else {
+			timeBetweenTweets = currentTime - previousTime;
+		}		
+
+		timeBuffer.push(timeBetweenTweets);
+
+		if (timeBuffer.length > BUFFER_LENGTH){
+			timeBuffer.splice(0,1);
+		}
+
+		for (i in timeBuffer){
+			rate += timeBuffer[i];
+		}
+
+		rate = Math.floor(rate/timeBuffer.length);
+		
+		console.log(rate);
+
+		return rate;
+}
